bc875f7d3f5d7a89d5c9eeb7a022acc6acc9e455
apamanoko <apamanoko221@yahoo.co.jp>
2025-08-09 18:19:07 +0800

    add: Authentication

--- app/provider.tsx
+++ app/provider.tsx
@@ -1,9 +1,40 @@
-import React from 'react'
+"use client"
+import React, { useContext, useEffect, useState } from 'react'
+import axios from 'axios'
+import { useUser } from '@clerk/nextjs';
+import { UserDetailContext } from '@/context/UserDetailContext';
 
-function Provider() {
-  return (
-    <div>Provider</div>
-  )
+export type UsersDetail = {
+  name: string,
+  email: string,
+  credits: number
 }
+
+function Provider({
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) {
+
+    const { user } = useUser();
+    const [userDetail,setUserDetail] = useState<any>();
+    useEffect(()=>{
+        user && CreateNewUser();
+    },[user])
+
+    const CreateNewUser = async () =>{
+      const result = await axios.post("/api/users");
+      console.log(result.data);
+      setUserDetail(result.data);
+    }
+    
+    return (
+      <div>
+        <UserDetailContext.Provider value={{userDetail, setUserDetail}}>
+          {children}
+        </UserDetailContext.Provider>
+      </div>
+    )
+}  
 
 export default Provider
