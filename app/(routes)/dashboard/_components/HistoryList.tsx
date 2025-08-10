82ccfe1930f1ba56abde306a5c43ac3e7a6f9238
apamanoko <apamanoko221@yahoo.co.jp>
2025-08-10 17:52:30 +0800

    add: Dashboard Home Screen (without large image files)

---
+++ app/(routes)/dashboard/_components/HistoryList.tsx
@@ -0,0 +1,23 @@
+"use client";
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
+import React, { useState } from 'react'
+
+function HistoryList() {
+    const [historyList, setHistoryList] = useState([]);
+  return (
+    <div className='mt-10'>
+        {historyList.length == 0 ? 
+            <div className='flex flex-col items-center justify-center p-7 border border-dashed rounded-2xl border-2'>
+                <Image src={"/medical-assistance.png"} alt="empty" width={150} height={150} />
+                <h2 className='font-bold text-xl mt-2'>No Recent Consultations</h2>
+                <p className='text-sm text-gray-500'>It looks like you haven't consulted with any doctors yet.</p>
+                <Button className='mt-3'>+ Start a Consultation</Button>
+            </div>
+         :  <div>List</div>
+        }
+    </div>
+  )
+}
+
+export default HistoryList
